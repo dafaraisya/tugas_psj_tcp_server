5452ea54938052b4600d2097394decfc3c539878
dafaraisya <dafaraisya.dr@gmail.com>
2021-11-15 12:05:32 +0700

    update arduino file

--- bacaSensorSuhu/bacaSensorSuhu.ino
+++ bacaSensorSuhu/bacaSensorSuhu.ino
@@ -3,14 +3,6 @@
 #include <OneWire.h>
 
 #define ONE_WIRE_BUS 4 //pin D2 nodemcu
-//#define rainAnalog A0
-//#define lightAnalog A0
-//#define rainDigital D0
-//int Value_D7;
-//int Value_D8;
-//int Pin_D7 = D7;
-//int Pin_D8 = D8;
-
 OneWire oneWire(ONE_WIRE_BUS);
 
 DallasTemperature sensors(&oneWire); // Pass the oneWire reference to Dallas Temperature.
@@ -32,9 +24,6 @@ void setup() {
 }
 
 void loop() {
-  //delay(1000);
-//  Serial.print("nilai sensor suhu = ");
-  //USE_SERIAL.println(getSensor());
   Serial.write(1);
   Serial.write(getSensor());
   delay(1000);
